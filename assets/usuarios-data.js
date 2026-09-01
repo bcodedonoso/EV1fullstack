eb85b3351915c2513f960b0c563c05dbc1071a15
bcodedonoso <ma.guajardod@duocuc.cl>
2026-08-31 20:49:44 -0400

    feat: admin creado con funcionalidades

---
+++ assets/usuarios-data.js
@@ -0,0 +1,5 @@
+const USUARIOS = [
+    { nombre: 'Admin', correo: 'admin@zthorken.cl', contrasena: 'admin123', tipo: 'administrador' },
+    { nombre: 'Juan', correo: 'juan@gmail.com', contrasena: 'juan123', tipo: 'cliente' },
+    { nombre: 'Maria', correo: 'maria@zthorken.cl', contrasena: 'maria123', tipo: 'cliente' }
+];
